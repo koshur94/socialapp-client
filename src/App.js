bd85c91f7600a5f5d052fe1fd3c4180d885d8c51
koshur94 <58008499+koshur94@users.noreply.github.com>
2020-05-18 15:55:42 +0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ import user from './pages/user';
 
 const theme = createMuiTheme(themeFile);
 
+axios.defaults.baseURL = 'https://europe-west2-hksocialapp.cloudfunctions.net/api';
+
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
